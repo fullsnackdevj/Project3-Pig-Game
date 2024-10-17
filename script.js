78c1e30e49e29f7085db0fcb4115c1d9b1bd9cf2
fullsnackdevj <jayfullsnackdev@gmail.com>
2024-10-17 13:05:23 +0800

    rolling the dice lecture

--- script.js
+++ script.js
@@ -3,9 +3,41 @@
 //selecting elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 // starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+const score = [0, 0];
+let currentScore = 0; // cannot be inside the function btnRoll because it'll reset every click.
+
+let activePlayer = 0;
+
+// rolling dice functionality
+
+btnRoll.addEventListener('click', function () {
+  // 1. Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  // 2. Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+  // 3. Check for rolled 1
+  if (dice !== 1) {
+    // add dice to current score
+    // currentScore = currentScore + dice; long Hand
+    currentScore += dice; // short hand
+    current0El.textContent = currentScore; //change later
+  } else {
+    currentScore = 0;
+    current0El.textContent = currentScore;
+    // switch to next player
+  }
+});
